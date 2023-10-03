36ef5a18a5f3a447ae24f889e2ecec29adaaacb8
Arthur LEMAIRE <arthur.lemaire@epitech.eu>
2023-10-03 10:08:03 +0200

    fix socket.io URL for Render deploy

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : `${process.env.REACT_APP_HOST}`;
+const URL = process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_HOST}` : `http://localhost:8000`;
 const socket = io(URL, {
     autoConnect: false
 });
